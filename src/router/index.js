904f18e255754ebea1d5d8b07e0421dcfc14891a
wxamlzm <zd5024@163.com>
2023-10-27 16:14:10 +0800

    UPDATE：添加测试页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: zd
+ * @Date: 2023-10-27 15:57:45
+ * @LastEditors: zd
+ * @LastEditTime: 2023-10-27 16:00:15
+ * @FilePath: \demo-vue\src\router\index.js
+ * @Description: 
+ */
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
@@ -16,7 +24,19 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/stressTestPage',
+    name: 'StressTestPage',
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(
+        /* webpackChunkName: "StressTestPage" */ '../views/stressTestPage/StressTestPage.vue'
+      )
   }
 ]
 
