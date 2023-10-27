c459beec818cf391b9a68e529f27768668afc20a
wxamlzm <zd5024@163.com>
2023-10-27 16:28:23 +0800

    UPDATE：引入element-ui

--- src/main.js
+++ src/main.js
@@ -3,6 +3,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+// element-ui引入
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css' // 默认主题
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
