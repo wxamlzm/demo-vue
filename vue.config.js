aa2f44b082069d7208fcb86357e503798ce1a8b9
wxamlzm <zd5024@163.com>
2023-10-31 16:13:55 +0800

    UPDATE：开发时不做elint报错

--- vue.config.js
+++ vue.config.js
@@ -2,12 +2,14 @@
  * @Author: zd
  * @Date: 2023-10-27 15:57:45
  * @LastEditors: zd
- * @LastEditTime: 2023-10-27 16:02:22
+ * @LastEditTime: 2023-10-31 09:45:55
  * @FilePath: \demo-vue\vue.config.js
  * @Description:
  */
 module.exports = {
   publicPath: './',
+
+  lintOnSave: false,
 
   css: {
     loaderOptions: {
