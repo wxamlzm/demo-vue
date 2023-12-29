342a544df630eeb216aac9454c5bdb9e52ac1591
wxamlzm <zd5024@163.com>
2023-12-29 15:42:51 +0800

    UPDATE：jest

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -2,13 +2,14 @@
  * @Author: zd
  * @Date: 2023-12-28 09:30:55
  * @LastEditors: zd
- * @LastEditTime: 2023-12-29 15:39:51
+ * @LastEditTime: 2023-12-29 15:42:43
  * @Description:
  */
 import Vuex from 'vuex'
 import { shallowMount, createLocalVue } from '@vue/test-utils'
 import HomeView from '@/views/HomeView.vue'
 
+// 这里我们在设法处理因为单组件的测试而引起的store无法在测试文件访问的问题
 const localVue = createLocalVue()
 localVue.use(Vuex)
 
