6c7096084ea81f000630336546b87b255db27af1
wxamlzm <zd5024@163.com>
2023-12-29 15:40:33 +0800

    UPDATE：jest更新例子

--- tests/unit/example.spec.js
+++ tests/unit/example.spec.js
@@ -1,9 +1,30 @@
-import { shallowMount } from '@vue/test-utils'
+/*
+ * @Author: zd
+ * @Date: 2023-12-28 09:30:55
+ * @LastEditors: zd
+ * @LastEditTime: 2023-12-29 15:39:51
+ * @Description:
+ */
+import Vuex from 'vuex'
+import { shallowMount, createLocalVue } from '@vue/test-utils'
 import HomeView from '@/views/HomeView.vue'
 
-describe('HomeView.vue', () => {
-  it('renders the correct text', () => {
-    const wrapper = shallowMount(HomeView)
-    expect(wrapper.text()).toBe('这是首页')
+const localVue = createLocalVue()
+localVue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    username: 'alice'
+  }
+})
+
+describe('HelloWorld.vue', () => {
+  it('renders test when passed', () => {
+    const msg = '刷新'
+    const wrapper = shallowMount(HomeView, {
+      store,
+      localVue
+    })
+    expect(wrapper.text()).toMatch(msg)
   })
 })
